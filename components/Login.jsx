d5db47def8f641005a11af5c8fd23106865b24de
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-02-13 16:23:14 +0200

    asd

--- components/Login.jsx
+++ components/Login.jsx
@@ -21,16 +21,16 @@ const Login = () => {
 
 
   return (
-    <Stack display={theme.flexBox} direction={isMobile ? "column" : "row"} height="100vh" justifyContent={theme.center} alignItems={theme.center}>
+    <Stack display={theme.flexBox} direction={isMobile ? "column" : "row"} height="100vh">
       <Stack width={isMobile ? "100vw" : "50vw"}>
-        <img id="hiking-image" src="/hiking.png" alt="hiking" width="100%" style={{objectFit: "cover", height:"100vh" }} />
+       { isMobile ? null : <img id="hiking-image" src="/hiking.png" alt="hiking" width="100%" style={{objectFit: "cover", height:"100vh" }} /> }
         <img id="logo" src="/logo.png" alt="logo" width={ isMobile ?  "90px" : "110px"} height={ isMobile ?  "60" : "70"} style={{ position: "absolute", marginLeft: isMobile ? "25px" : "50px", paddingTop: isMobile ? "22px" : "16px" }}/>  
       </Stack>
 
 
     <Stack width={ isMobile ? "100vw" : "50vw"} display={theme.flexBox} justifyContent={theme.center} alignItems={theme.center}>
 
-    <Stack pl={8} pr={8} pt={10} pb={10} borderRadius={ isMobile ? "0px" :"10px"} backgroundColor="#f4f4f4" width={isMobile ? "100vw" : null} display={theme.flexBox} justifyContent={theme.center} alignItems={theme.center}>
+    <Stack pl={8} pr={8} pt={10} pb={10} borderRadius={ isMobile ? "0px" :"10px"} backgroundColor="#f4f4f4" width={isMobile ? "100vw" : null} height={isMobile ? "100vh" : null} display={theme.flexBox} justifyContent={theme.center} alignItems={theme.center}>
     <Typography component="h1" variant='span' fontSize="2.4em" mb={4}>Log in to Planner</Typography>
 
     <button className='login-button' onClick={loginWithGoogle}> <Stack width="220px" margin="0 auto" display={theme.flexBox} direction="row" alignItems={theme.center}><img src="/google.png" width="23px" style={{marginRight: "15px"}} alt="google" />Continue with Google </Stack></button>
