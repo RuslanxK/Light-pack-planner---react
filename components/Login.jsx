fdcef62969c51f99c199d3187a2f2a6121132614
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-02-13 15:10:26 +0200

    sa

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,12 +1,13 @@
 "use client"
 
-import { Stack, Button, Typography } from '@mui/material'
+import { Stack, Typography, useMediaQuery } from '@mui/material'
 import { useTheme } from '@emotion/react';
 import { signIn } from "next-auth/react"
 
 const Login = () => {
 
   const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
   const loginWithGoogle = () => {
     signIn('google', { callbackUrl: '/' })
@@ -20,14 +21,16 @@ const Login = () => {
 
 
   return (
-    <Stack display={theme.flexBox} direction="row">
-      <Stack width="50vw">
-        <img id="hiking-image" src="/hiking.png" alt="hiking" style={{ height: "100vh", objectFit: "cover"}} />
-        <img id="logo" src="/logo.png" alt="logo" width="110px" height="70" style={{ position: "absolute", marginLeft: "50px", paddingTop: "16px" }}/>  
+    <Stack display={theme.flexBox} direction={isMobile ? "column" : "row"}>
+      <Stack width={isMobile ? "100vw" : "50vw"}>
+        <img id="hiking-image" src="/hiking.png" alt="hiking" style={{ height: isMobile ? "500px": "100vh", objectFit: "cover"}} />
+        <img id="logo" src="/logo.png" alt="logo" width={ isMobile ?  "80px" : "110px"} height={ isMobile ?  "50" : "70"} style={{ position: "absolute", marginLeft: "50px", paddingTop: "16px" }}/>  
       </Stack>
-      <Stack width="50vw" display={theme.flexBox} justifyContent={theme.center} alignItems={theme.center}>
 
-    <Stack pl={8} pr={8} pt={10} pb={10} borderRadius="10px" backgroundColor="#f4f4f4">
+
+    <Stack width={ isMobile ? "100vw" : "50vw"} display={theme.flexBox} justifyContent={theme.center} alignItems={theme.center}>
+
+    <Stack pl={8} pr={8} pt={ isMobile ? 5 : 10} pb={ isMobile ?  5  : 10} borderRadius={ isMobile ? "0px" :"10px"} backgroundColor="#f4f4f4" width={isMobile ? "100%" : null} display={theme.flexBox} justifyContent={theme.center} alignItems={theme.center}>
     <Typography component="h1" variant='span' fontSize="2.4em" mb={4}>Log in to Planner</Typography>
 
     <button className='login-button' onClick={loginWithGoogle}> <Stack width="220px" margin="0 auto" display={theme.flexBox} direction="row" alignItems={theme.center}><img src="/google.png" width="23px" style={{marginRight: "15px"}} alt="google" />Continue with Google </Stack></button>
