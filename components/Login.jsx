cb98fc836c9227e8c17724e2cc72c97ba792f38a
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-02-15 13:25:19 +0200

    asdas

--- components/Login.jsx
+++ components/Login.jsx
@@ -35,11 +35,10 @@ const handleSubmit = (e) => {
 
     <div class="login-content">
     <Stack display={theme.flexBox} justifyContent={theme.center} alignItems={theme.center}>
-
-    <form onSubmit={handleSubmit} style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", backgroundColor:"rgba(255,255,255, 0.4)", padding: "30px", borderRadius:"10px"}}>
-    <Typography component="h1" variant='span' fontSize="2.2em" mb={3}>Log in to Planner</Typography>
-    <TextField label="Email" name="email" sx={{marginBottom: "15px", background: "white", width: "300px", borderRadius: "7px"}} />
-    <TextField label="Password" name='password' sx={{marginBottom: "15px", background: "white", width: "300px", borderRadius: "7px"}} />
+    <form onSubmit={handleSubmit} style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", backgroundColor:"rgba(255,255,255, 0.5)", padding: "20px", borderRadius:"10px"}}>
+    <Typography component="h1" variant='span' fontSize="2em" mb={3}>Log in to Planner</Typography>
+    <TextField label="Email" name="email" sx={{marginBottom: "15px", background: "white", width: "280px", borderRadius: "7px"}} />
+    <TextField label="Password" name='password' sx={{marginBottom: "15px", background: "white", width: "280px", borderRadius: "7px"}} />
     <button class="login-button-regular">Log in</button>
 
     <button className='login-button' onClick={loginWithGoogle}> <Stack width="220px" margin="0 auto" display={theme.flexBox} direction="row" alignItems={theme.center}><img src="/google.png" width="23px" style={{marginRight: "15px"}} alt="google" />Continue with Google </Stack></button>
