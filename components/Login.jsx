f408b0e3e1f5e147fd0ceb68c98e2ec97fbc5d78
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-02-15 14:43:54 +0200

    asdasd

--- components/Login.jsx
+++ components/Login.jsx
@@ -30,13 +30,13 @@ const handleSubmit = (e) => {
       
       <div class="login-img">
         <img id="hiking-image" src="/hiking.png" alt="hiking" width="100%" style={{objectFit: "cover", height:"100vh" }} /> 
-        <img id="logo" src="/logo.png" alt="logo" width="110px" height="70" style={{ position: "absolute", top: "16px", left: '25px' }}/> 
+        <img id="logo" src="/logo.png" alt="logo" width="110px" height="70" style={{ position: "absolute", top: "16px", left: '35px' }}/> 
       </div>
 
 
     <div class="login-content">
-    <img id="logo" src="/logo.png" alt="logo" width="110px" height="70" style={{ position: "absolute", top: "16px", left: '25px' }}/> 
-    <Stack display={theme.flexBox} justifyContent={theme.center} alignItems={theme.center} backgroundColor="rgba(255,255,255, 0.4)" borderRadius="7px" height="250px" mb={3} p={3}>
+    <img id="logo-mobile-login" src="/logo.png" alt="logo" width="110px" height="70" style={{ position: "absolute", top: "16px", left: '25px' }}/> 
+    <Stack display={theme.flexBox} justifyContent={theme.center} backgroundColor="rgba(255,255,255, 0.4)" borderRadius="7px" height="200px" mb={8} p={3}>
     <h1 className='login-text'>Log in to Planner</h1>
     {/* <form onSubmit={handleSubmit} style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", backgroundColor:"rgba(255,255,255, 0.5)", padding: "20px", borderRadius:"10px"}}>
     <TextField label="Email" name="email" sx={{marginBottom: "15px", background: "white", width: "280px", borderRadius: "7px"}} />
