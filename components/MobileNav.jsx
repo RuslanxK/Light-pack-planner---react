75911d2cf741a50171dcba3c04dddf3ff3745f71
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-02-15 15:05:43 +0200

    asd

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,11 +1,15 @@
 import {IconButton, Stack} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import { useRouter } from 'next/navigation';
 
 const MobileNav = () => {
+
+ const router = useRouter()
+
   return (
      <div class="menu-mobile">
-         <img src='./logo.png' width="90px" height="58px" class="logo-mobile" />
+         <img src='./logo.png' width="90px" height="58px" class="logo-mobile" onClick={() => router.push('/')} />
 
          <Stack display="flex" direction="row">
          <IconButton><MenuIcon /></IconButton>
